2269691487c4350ed682f350b93a44be6c54c40a
Muhammed Faisal <faisalirulam@gmail.com>
2021-09-25 20:17:31 +0530

    updated

--- first/settings.py
+++ first/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#zv7hf_dh&_r(t)rgi3pe79l@%ov-7!6dsxx&+k2uhzonz)9d7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
